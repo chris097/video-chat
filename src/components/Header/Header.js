707b08e977366b34e00481d190535e989b864fc8
chris097 <chrisfidel.international@gmail.com>
2023-04-20 13:19:18 +0100

    added message feature

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
       <div className='h-20 w-full fixed top-0 left-0 z-50 bg-[#1a1a1a] border-b border-gray-700 text-white'>
           <div className='flex justify-between w-full items-center px-10 h-full'>
               <div className='text-3xl font-medium'>Talent+</div> 
-          <button onClick={copyText} className='bg-[#845695] text-white px-3 py-2 rounded-md'>Copy Link</button>
+          <button onClick={copyText} className='bg-[#845695] text-white px-3 py-2 rounded-md'>Copy to invite a friend</button>
           </div>
       </div>
       </>
